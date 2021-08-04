560ac79e81f4691e164b833125c2d26156183523
1aerostorm <aerostorm1@gmail.com>
2021-08-05 00:12:32 +0300

    Register event plugin

--- programs/golosd/main.cpp
+++ programs/golosd/main.cpp
@@ -27,6 +27,7 @@
 #include <golos/plugins/operation_dump/operation_dump_plugin.hpp>
 #include <golos/plugins/worker_api/worker_api_plugin.hpp>
 #include <golos/plugins/elastic_search/elastic_search_plugin.hpp>
+#include <golos/plugins/event_plugin/event_plugin.hpp>
 #ifdef MONGODB_PLUGIN_BUILT
     #include <golos/plugins/mongo_db/mongo_db_plugin.hpp>
 #endif
@@ -87,6 +88,7 @@ namespace golos {
             appbase::app().register_plugin<golos::plugins::operation_dump::operation_dump_plugin>();
             appbase::app().register_plugin<golos::plugins::worker_api::worker_api_plugin>();
             appbase::app().register_plugin<golos::plugins::elastic_search::elastic_search_plugin>();
+            appbase::app().register_plugin<golos::plugins::event_plugin::event_plugin>();
             #ifdef MONGODB_PLUGIN_BUILT
                 appbase::app().register_plugin<golos::plugins::mongo_db::mongo_db_plugin>();
             #endif
