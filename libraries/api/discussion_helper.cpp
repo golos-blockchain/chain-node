36caa1d479ec3901cfa312b8fc3e71f643ca5fe2
1aerostorm <aerostorm1@gmail.com>
2021-08-09 07:34:23 +0300

    Add min_golos_power_to_curate to comment API object #162

--- libraries/api/discussion_helper.cpp
+++ libraries/api/discussion_helper.cpp
@@ -124,6 +124,7 @@ namespace golos { namespace api {
         d.auction_window_reward_destination = o.auction_window_reward_destination;
         d.auction_window_size = o.auction_window_size;
         d.curation_rewards_percent = o.curation_rewards_percent;
+        d.min_golos_power_to_curate = o.min_golos_power_to_curate;
 
         for (auto& route : o.beneficiaries) {
             d.beneficiaries.push_back(route);
