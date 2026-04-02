fb0f957ab3a979fd6967331bcfa861f4b30fbe2c
1aerostorm <aerostorm1@gmail.com>
2026-04-02 21:44:07 +0300

    Upgrate tests global fixture approach (old was obsolete)

--- tests/plugin_tests/main.cpp
+++ tests/plugin_tests/main.cpp
@@ -34,16 +34,22 @@
 
 extern uint32_t STEEMIT_TESTING_GENESIS_TIMESTAMP;
 
-boost::unit_test::test_suite *init_unit_test_suite(int argc, char *argv[]) {
-    fc::configure_logging(fc::logging_config::default_config(fc::log_level::error));
-    std::srand(time(NULL));
-    std::cout << "Random number generator seeded to " << time(NULL)
-              << std::endl;
-    const char *genesis_timestamp_str = getenv("STEEMIT_TESTING_GENESIS_TIMESTAMP");
-    if (genesis_timestamp_str != nullptr) {
-        STEEMIT_TESTING_GENESIS_TIMESTAMP = std::stoul(genesis_timestamp_str);
+struct GlobalFixture {
+    GlobalFixture() {
+        fc::configure_logging(fc::logging_config::default_config(fc::log_level::error));
+        std::srand(time(NULL));
+        std::cout << "Random number generator seeded to " << time(NULL)
+                  << std::endl;
+        const char *genesis_timestamp_str = getenv("STEEMIT_TESTING_GENESIS_TIMESTAMP");
+        if (genesis_timestamp_str != nullptr) {
+            STEEMIT_TESTING_GENESIS_TIMESTAMP = std::stoul(genesis_timestamp_str);
+        }
+        std::cout << "STEEMIT_TESTING_GENESIS_TIMESTAMP is "
+                  << STEEMIT_TESTING_GENESIS_TIMESTAMP << std::endl;
     }
-    std::cout << "STEEMIT_TESTING_GENESIS_TIMESTAMP is "
-              << STEEMIT_TESTING_GENESIS_TIMESTAMP << std::endl;
-    return nullptr;
-}
+
+    ~GlobalFixture() {
+    }
+};
+
+BOOST_TEST_GLOBAL_FIXTURE(GlobalFixture);
